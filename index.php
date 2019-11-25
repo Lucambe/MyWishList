7b9e40ab9a057f05dbd221f48ca3cfb9917b5e15
Jules Sayer <jules.sayer@protonmail.com>
2019-11-25 21:00:39 +0100

    Ajout des routes

--- index.php
+++ index.php
@@ -1,50 +1,29 @@
 <?php
-require('vendor/autoload.php');
-use Illuminate\Database\Capsule\Manager as DB;
-use mywishlist\models\Liste as Liste;
-use mywishlist\models\Item as Item;
-$db = new DB();
-$db->addConnection(parse_ini_file("src/config/database.ini"));
-$db->setAsGlobal();
-$db->bootEloquent();
+require_once (__DIR__ . '/vendor/autoload.php');
 
-
-// Lister les listes de souhaits
-$listes = Liste::get();
-foreach($listes as $liste) {
-    echo $liste->no . ": " . $liste->titre . " - " . $liste->description . "<br/>";
+$db = new \Illuminate\Database\Capsule\Manager();
+if(file_exists("src/config/database.ini")) {
+    $db->addConnection(parse_ini_file("src/config/database.ini"));
+    $db->setAsGlobal();
+    $db->bootEloquent();
+} else {
+    throw new Exception('Le fichier src/config/database.ini n\'existe pas');
 }
 
-echo "<br/><br/>";
+$app = new \Slim\Slim();
 
-// Lister les items
-$items = Item::get();
-foreach($items as $item) {
-    echo $item->id . ": " . $item->nom . " - " . $item->descr . "<br/>";
-}
+$app->get('/', function () {
+    echo "Hello, World !";
+});
 
-echo "<br/><br/>";
+$app->get('/items', function() {
+    $c = new \mywishlist\controllers\ItemController();
+    $c->getItems();
+});
 
-// Afficher un item en particulier, dont l'id est passé en paramêtre dans l'url (test.php?id=1)
-if(isset($_GET['id']) && !empty($_GET['id']) && is_numeric($_GET['id'])) {
-    $i = Item::where('id', '=', $_GET['id'])->first();
-    if($i != null) {
-        echo $i->id . ": " . $i->nom . " - " . $i->descr . "<br/>";
-    } else {
-        echo "L'id n'existe pas";
-    }
-}
+$app->get('/item/:id', function($id) {
+    $c = new \mywishlist\controllers\ItemController();
+    $c->getItem($id);
+});
 
-echo "<br/><br/>";
-
-// Créer un item, l'insérer dans la base et l'ajouter à une liste de souhaits.
-
-$i2 = new Item();
-$i2->liste_id = 5;
-$i2->nom = "Test";
-$i2->descr = "Test";
-$i2->img = "undostres.jpg";
-$i2->tarif = 667.69;
-if($i2->save()) {
-    echo "Ajout OK";
-}
+$app->run();
