34d865d4076e48cc280bbb409458902b1530b007
Jules Sayer <jules.sayer@protonmail.com>
2019-12-11 14:55:31 +0100

    Suppression fomulaire liste

--- src/controllers/MessageController.php
+++ src/controllers/MessageController.php
@@ -10,5 +10,7 @@ class MessageController extends Controller {
      * @todo: Fonction ajouter message a partir de données POST
      */
     public function addMessage($request, $response, $args) {
+        $name = $request->getParsedBody()['name'];
+        $message = $request->getParsedBody()['message'];
     }
 }
