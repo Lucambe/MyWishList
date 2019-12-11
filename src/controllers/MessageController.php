d6085658142a6e4ecb759ca45a7bbbc80d7b7e47
Nathan <nat.che@hotmail.fr>
2019-12-11 16:18:35 +0100

    Ajout de la fonctionnalité d'ajout de message (fonctionnel)

--- src/controllers/MessageController.php
+++ src/controllers/MessageController.php
@@ -4,13 +4,38 @@
 namespace mywishlist\controllers;
 
 
+use Dflydev\FigCookies\FigResponseCookies;
+use Dflydev\FigCookies\SetCookie;
+use mywishlist\models\Liste;
+use mywishlist\models\Message;
+
 class MessageController extends Controller {
 
     /**
      * @todo: Fonction ajouter message a partir de données POST
      */
     public function addMessage($request, $response, $args) {
-        $name = $request->getParsedBody()['name'];
-        $message = $request->getParsedBody()['message'];
+        try {
+            $name = $request->getParsedBody()['name'];
+            $message = $request->getParsedBody()['message'];
+            $token = $request->getParsedBody()['token'];
+            $liste = Liste::where('token', '=', $token)->first();
+            if(is_null($liste)) {
+                throw new Exception();
+            }
+
+            $m = new Message();
+            $m->idListe = $liste->no;
+            $m->message = $message;
+            $m->messager = $name;
+            $m->save();
+            $response = FigResponseCookies::set($response, SetCookie::create("nom")->withValue($name)->rememberForever());
+            $this->flash->addMessage('success', "$name, Votre message a été envoyé");
+            $response = $response->withRedirect($this->router->pathFor('home'));
+        } catch(Exception $e) {
+            $this->flash->addMessage('error', 'Nous n\'avons pas pu envoyer votre message.');
+            $response = $response->withRedirect($this->router->pathFor('home'));
+        }
+        return $response;
     }
 }
