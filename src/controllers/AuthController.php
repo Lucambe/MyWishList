5fedaea925d3294025c493f800120c06687ab90d
Jules Sayer <jules.sayer@protonmail.com>
2020-01-10 14:39:59 +0100

    Modification login/register

---
+++ src/controllers/AuthController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace mywishlist\controllers;
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+class AuthController extends Controller {
+
+    public function login(Request $request, Response $response, array $args): Response {
+        return $response;
+    }
+
+    public function register(Request $request, Response $response, array $args): Response {
+        return $response;
+    }
+
+    public function logout(Request $request, Response $response, array $args): Response {
+        return $response;
+    }
+
+}
