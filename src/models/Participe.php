9f702d0762c5857add1138ea32f64c0110fa6d88
Nathan <nat.che@hotmail.fr>
2020-01-15 00:12:04 +0100

    merge model participe

--- src/models/Participe.php
+++ src/models/Participe.php
@@ -7,21 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Participe extends Model {
     public $timestamps = false;
     protected $table = "participe";
-<<<<<<< HEAD
+
     protected $primaryKey = "id";
-=======
-    protected $primaryKey = "idPart";
->>>>>>> master
 
     public function cagnotte() {
         return $this->belongsTo('\mywishlist\models\Cagnotte', 'id');
     }
-<<<<<<< HEAD
-=======
-
-    public function user() {
-        return $this->hasOne('\mywishlist\models\User', 'id');
-    }
-
->>>>>>> master
 }
